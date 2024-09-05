dc0a7211e8a50bdce68c749250722e6e3d7e19c5
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-05 18:53:03 -0500

    Add implementation EpsController in endpoint getAllEps

--- src/main/java/com/urgenciasYa/controller/interfaces/IModelEps.java
+++ src/main/java/com/urgenciasYa/controller/interfaces/IModelEps.java
@@ -1,10 +1,11 @@
 package com.urgenciasYa.controller.interfaces;
 
+import com.urgenciasYa.dto.response.EpsDTO;
 import com.urgenciasYa.model.Eps;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface IModelEps {
-    public ResponseEntity<List<Eps>> getAllEps();
+    public ResponseEntity<List<EpsDTO>> getAllEps();
 }
