a1260997f2c3ad0b8ce70ace6a7610aed9c48268
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-05 00:31:41 -0500

    create ORM hospitals table

---
+++ src/main/java/com/urgenciasYa/model/Eps.java
@@ -0,0 +1,21 @@
+package com.urgenciasYa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Eps {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+}
