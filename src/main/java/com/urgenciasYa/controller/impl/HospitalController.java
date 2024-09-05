8ab255aa346066ff72f875984289933ed8df3751
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-05 15:39:04 -0500

    create the hospital controller and modify the hospital repository

---
+++ src/main/java/com/urgenciasYa/controller/impl/HospitalController.java
@@ -0,0 +1,34 @@
+package com.urgenciasYa.controller.impl;
+
+import com.urgenciasYa.model.Hospital;
+import com.urgenciasYa.service.Impl.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hospital")
+public class HospitalController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @GetMapping("/findByEpsAndTown")
+    public ResponseEntity<List<Hospital>> getHospitalByEpsAndTown(
+            @RequestParam String eps,
+            @RequestParam String town
+    ){
+        List<Hospital> hospitals = hospitalService.findHospitalByEpsAndTown(eps, town);
+        if(hospitals.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(hospitals, HttpStatus.OK);
+    }
+}
--- src/main/java/com/urgenciasYa/repository/HospitalRepository.java
+++ src/main/java/com/urgenciasYa/repository/HospitalRepository.java
@@ -2,6 +2,7 @@ package com.urgenciasYa.repository;
 
 import com.urgenciasYa.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,11 @@ import java.util.List;
 
 @Repository
 public interface HospitalRepository extends JpaRepository<Integer, Hospital> {
+
+    @Query("SELECT h FROM Hospital h " +
+            "JOIN h.eps_id e " +
+            "JOIN h.town_id t " +
+            "WHERE e.name = :epsName AND t.name = :townName")
     List<Hospital> findByEpsAndTown(@Param("epsName") String epsName, @Param("townName") String townName);
 
 }
