f41cde96175c77060bea2fbebc4ff0ab90cad316
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-12 15:32:45 -0500

    Add service implementation

--- src/main/java/com/urgenciasYa/service/IModel/ITownsModel.java
+++ src/main/java/com/urgenciasYa/service/IModel/ITownsModel.java
@@ -1,7 +1,9 @@
 package com.urgenciasYa.service.IModel;
 
+import com.urgenciasYa.dto.response.TownsDTO;
 import com.urgenciasYa.model.Towns;
+import com.urgenciasYa.service.crud.CreateDTO;
 import com.urgenciasYa.service.crud.ReadAll;
 
-public interface ITownsModel extends ReadAll<Towns> {
+public interface ITownsModel extends ReadAll<Towns>, CreateDTO<TownsDTO,Towns> {
 }
--- src/main/java/com/urgenciasYa/service/crud/Create.java
+++
@@ -1,7 +0,0 @@
-package com.urgenciasYa.service.crud;
-
-import com.urgenciasYa.dto.request.UserRegisterDTO;
-
-public interface Create <Entity> {
-    public Entity create(UserRegisterDTO userRegisterDTO);
-}
