500e8c980cf17031f1511f0175fe93551170e595
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-14 00:22:27 -0500

    create the imodel to hospital controller

---
+++ src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
@@ -0,0 +1,7 @@
+package com.urgenciasYa.controller.interfaces;
+
+import com.urgenciasYa.controller.generic.Create;
+import com.urgenciasYa.dto.request.HospitalCreateResponseDTO;
+
+public interface IModelHospital extends Create<HospitalCreateResponseDTO> {
+}
