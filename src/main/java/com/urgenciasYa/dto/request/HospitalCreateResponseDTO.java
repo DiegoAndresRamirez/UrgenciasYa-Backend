899c6c17822bc443e8c7d63da219cd3498a2af6c
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-13 21:07:23 -0500

    update the dto hospital

--- src/main/java/com/urgenciasYa/dto/request/HospitalCreateResponseDTO.java
+++ src/main/java/com/urgenciasYa/dto/request/HospitalCreateResponseDTO.java
@@ -15,29 +15,16 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HospitalCreateResponseDTO {
-    @Column(nullable = false)
     private String url_image;
-    @Column(length = 15, nullable = false)
     private String phone_number;
-    @Column(length = 225, nullable = false)
     private String name;
-    @Column(nullable = false)
     private Float rating;
-    @Column(nullable = false)
     private Integer morning_peak;
-    @Column(nullable = false)
     private Integer afternoon_peak;
-    @Column(nullable = false)
     private Integer night_peak;
-    @Column(nullable = false)
     private String howtogetthere;
-    @JoinColumn( nullable = false)
-    @OneToOne
-    private Towns town_id;
-    @OneToMany(mappedBy = "hospital")
+    private Towns town_id; 
     private List<Eps> eps_id;
-    @Column(nullable = false)
     private Integer latitude;
-    @Column(nullable = false)
     private Integer longitude;
 }
