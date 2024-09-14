80e333e673b844f4f19cb2fcb45ae118ac97570c
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-14 00:58:04 -0500

    add the method to delete in the imodel to controller

--- src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
+++ src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
@@ -1,8 +1,9 @@
 package com.urgenciasYa.controller.interfaces;
 
 import com.urgenciasYa.controller.generic.Create;
+import com.urgenciasYa.controller.generic.Delete;
 import com.urgenciasYa.controller.generic.Update;
 import com.urgenciasYa.dto.request.HospitalCreateResponseDTO;
 
-public interface IModelHospital extends Create<HospitalCreateResponseDTO> , Update<HospitalCreateResponseDTO, Long> {
+public interface IModelHospital extends Create<HospitalCreateResponseDTO> , Update<HospitalCreateResponseDTO, Long> , Delete<Long> {
 }
