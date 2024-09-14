c12f0e7646d0d952db06a09ed38cb6a844cc8f98
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-14 02:25:35 -0500

    add the method getAl in the Imodel hospital controller

--- src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
+++ src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
@@ -1,11 +1,8 @@
 package com.urgenciasYa.controller.interfaces;
 
-import com.urgenciasYa.controller.generic.Create;
-import com.urgenciasYa.controller.generic.Delete;
-import com.urgenciasYa.controller.generic.GetById;
-import com.urgenciasYa.controller.generic.Update;
+import com.urgenciasYa.controller.generic.*;
 import com.urgenciasYa.dto.request.HospitalCreateResponseDTO;
 import com.urgenciasYa.model.Hospital;
 
-public interface IModelHospital extends Create<HospitalCreateResponseDTO> , Update<HospitalCreateResponseDTO, Long> , Delete<Long>, GetById<Hospital, Long> {
+public interface IModelHospital extends Create<HospitalCreateResponseDTO> , Update<HospitalCreateResponseDTO, Long> , Delete<Long>, GetById<Hospital, Long> , GetAll<Hospital> {
 }
