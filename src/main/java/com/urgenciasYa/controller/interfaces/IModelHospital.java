8fb4e249f15b315275cd8ae9c39a7cd401e4804d
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-14 00:45:38 -0500

    add the method update in the imodel controller hospital

--- src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
+++ src/main/java/com/urgenciasYa/controller/interfaces/IModelHospital.java
@@ -1,7 +1,8 @@
 package com.urgenciasYa.controller.interfaces;
 
 import com.urgenciasYa.controller.generic.Create;
+import com.urgenciasYa.controller.generic.Update;
 import com.urgenciasYa.dto.request.HospitalCreateResponseDTO;
 
-public interface IModelHospital extends Create<HospitalCreateResponseDTO> {
+public interface IModelHospital extends Create<HospitalCreateResponseDTO> , Update<HospitalCreateResponseDTO, Long> {
 }
