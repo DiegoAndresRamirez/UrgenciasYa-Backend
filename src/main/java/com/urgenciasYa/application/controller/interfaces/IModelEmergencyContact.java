52c602f9aa56a12f1874d98456f6f263d5c8d88b
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-15 00:43:44 -0500

    add the method update in the imodel to EmergencyContact controller

--- src/main/java/com/urgenciasYa/application/controller/interfaces/IModelEmergencyContact.java
+++ src/main/java/com/urgenciasYa/application/controller/interfaces/IModelEmergencyContact.java
@@ -1,7 +1,8 @@
 package com.urgenciasYa.application.controller.interfaces;
 
 import com.urgenciasYa.application.controller.generic.Create;
+import com.urgenciasYa.application.controller.generic.Update;
 import com.urgenciasYa.application.dto.request.EmergencyContactRequestDTO;
 
-public interface IModelEmergencyContact extends Create<EmergencyContactRequestDTO> {
+public interface IModelEmergencyContact extends Create<EmergencyContactRequestDTO> , Update<EmergencyContactRequestDTO, Long> {
 }
