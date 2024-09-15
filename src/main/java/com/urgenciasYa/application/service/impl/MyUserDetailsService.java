9c94f8cdf944b7adb63959dd2ace57bced04eef8
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-15 02:08:21 -0500

    create UserPrincipal

--- src/main/java/com/urgenciasYa/application/service/impl/MyUserDetailsService.java
+++ src/main/java/com/urgenciasYa/application/service/impl/MyUserDetailsService.java
@@ -1,17 +1,21 @@
 package com.urgenciasYa.application.service.impl;
 
 import com.urgenciasYa.domain.model.UserEntity;
+import com.urgenciasYa.domain.model.UserPrincipal;
 import com.urgenciasYa.infrastructure.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public class MyUserDetailsService implements UserdetailService {
+public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username);
+        UserEntity user = userRepository.findByName(username);
         if (user == null) {
             System.out.println("User Not Found");
             throw new UsernameNotFoundException("user not found");
