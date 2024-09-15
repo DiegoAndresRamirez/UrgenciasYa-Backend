e5d7ac57c32173ebb8235563cfa2954922018cb9
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-15 04:04:33 -0500

    add the configuration to manage twilio

---
+++ src/main/java/com/urgenciasYa/infrastructure/config/twilio/TwilioConfig.java
@@ -0,0 +1,20 @@
+package com.urgenciasYa.infrastructure.config.twilio;
+
+import com.twilio.Twilio;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioConfig {
+    @Value("${twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth-token}")
+    private String authToken;
+
+    @Bean
+    public void initializeTwilio() {
+        Twilio.init(accountSid, authToken);
+    }
+}
