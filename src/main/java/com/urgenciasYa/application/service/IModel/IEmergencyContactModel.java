73175471781572725f95cb63450d0fed31be3265
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-15 00:52:31 -0500

    add the method delete in the imodel to the service//error fixed

--- src/main/java/com/urgenciasYa/application/service/IModel/IEmergencyContactModel.java
+++ src/main/java/com/urgenciasYa/application/service/IModel/IEmergencyContactModel.java
@@ -2,9 +2,10 @@ package com.urgenciasYa.application.service.IModel;
 
 import com.urgenciasYa.application.dto.request.EmergencyContactRequestDTO;
 import com.urgenciasYa.application.service.crud.CreateDTO;
+import com.urgenciasYa.application.service.crud.Delete;
 import com.urgenciasYa.application.service.crud.Update;
 import com.urgenciasYa.domain.model.EmergencyEntity;
 
-public interface IEmergencyContactModel extends CreateDTO<EmergencyContactRequestDTO, EmergencyEntity>{
+public interface IEmergencyContactModel extends CreateDTO<EmergencyContactRequestDTO, EmergencyEntity>, Delete<Long> {
     EmergencyEntity Update(Long id, EmergencyContactRequestDTO emergencyContactRequestDTO);
 }
