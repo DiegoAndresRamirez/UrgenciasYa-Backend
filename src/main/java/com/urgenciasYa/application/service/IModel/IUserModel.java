015911efd677927b7027baeb49d74d201a3923eb
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-16 00:38:04 -0500

    add the readAll method in the imodel to userService

--- src/main/java/com/urgenciasYa/application/service/IModel/IUserModel.java
+++ src/main/java/com/urgenciasYa/application/service/IModel/IUserModel.java
@@ -1,9 +1,10 @@
 package com.urgenciasYa.application.service.IModel;
 
 import com.urgenciasYa.application.dto.request.UserRegisterDTO;
+import com.urgenciasYa.application.dto.response.UserResponseDTO;
+import com.urgenciasYa.application.service.crud.ReadAll;
 import com.urgenciasYa.domain.model.UserEntity;
 import com.urgenciasYa.application.service.crud.CreateDTO;
 
-public interface IUserModel extends CreateDTO<UserRegisterDTO,UserEntity> {
-//    UserEntity create(UserRegisterDTO userRegisterDTO);
+public interface IUserModel extends CreateDTO<UserRegisterDTO,UserEntity> , ReadAll<UserResponseDTO> {
 }
