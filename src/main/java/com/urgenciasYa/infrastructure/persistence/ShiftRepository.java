5f79cae109343159e1fc2e8c4251ab208c353324
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-24 08:31:05 -0500

    add new methods in the repository shift

--- src/main/java/com/urgenciasYa/infrastructure/persistence/ShiftRepository.java
+++ src/main/java/com/urgenciasYa/infrastructure/persistence/ShiftRepository.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
-    List<Shift> findByEstimatedTimeBeforeAndStatus(LocalDateTime estimatedTime, StatusShift status);
+    List<Shift> findAllByUserId(Long userId);
 
+    Optional<Shift> findByIdAndUserId(Long id, Long userId);
 }
