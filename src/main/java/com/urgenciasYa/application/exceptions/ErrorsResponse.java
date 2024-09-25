03840b68d0545a66c474624d29d7195659beff3e
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-25 03:44:07 -0500

    create the comment of the package exceptions and delete useless imports

--- src/main/java/com/urgenciasYa/application/exceptions/ErrorsResponse.java
+++ src/main/java/com/urgenciasYa/application/exceptions/ErrorsResponse.java
@@ -3,12 +3,16 @@ package com.urgenciasYa.application.exceptions;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
 import java.util.List;
+
+/*
+ * Represents a detailed error response that can contain multiple error messages.
+ * This class extends from ErrorSimple, allowing it to inherit basic error properties.
+ */
 
 @Getter
 @Setter
 @SuperBuilder
 public class ErrorsResponse extends ErrorSimple {
-    private List<String> errors;
+    private List<String> errors; // A list of error messages providing more detail on the issues encountered
 }
