918788efc2d2255e0d95efa1f63ebd142d7b1e73
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-25 00:38:19 -0500

    create the comment of the package twiliocontroller

--- src/main/java/com/urgenciasYa/application/controller/impl/twilioController/SmsController.java
+++ src/main/java/com/urgenciasYa/application/controller/impl/twilioController/SmsController.java
@@ -8,19 +8,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Controller for managing SMS operations using Twilio.
+ * This class handles HTTP requests related to sending SMS messages.
+ */
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/sms")
 public class SmsController {
     private final SmsService smsService;
 
+    //Constructs an SmsController with the specified SmsService.
+
     @Autowired
     public SmsController(SmsService smsService) {
         this.smsService = smsService;
     }
 
+    // Sends an SMS message to the specified phone number.
+
     @PostMapping
     public void sendSms(@RequestParam String phoneNumber, @RequestParam String message) {
         smsService.sendSms(phoneNumber, message);
     }
+
+    // still in development.
 }
