956997e381bdba0b655065f355b1219b45b606cc
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-25 01:22:21 -0500

    Add implementation Mapper method create

--- src/main/java/com/urgenciasYa/application/mappers/UserMapper.java
+++ src/main/java/com/urgenciasYa/application/mappers/UserMapper.java
@@ -3,6 +3,8 @@ package com.urgenciasYa.application.mappers;
 import com.urgenciasYa.application.dto.request.UserRegisterDTO;
 import com.urgenciasYa.application.dto.request.UserRegisterRequestDTO;
 import com.urgenciasYa.application.dto.request.UserUpdateDTO;
+import com.urgenciasYa.application.dto.response.EpsResponseDTO;
+import com.urgenciasYa.application.dto.response.EpsUserResponseDTO;
 import com.urgenciasYa.application.dto.response.LoginDTO;
 import com.urgenciasYa.application.dto.response.UserResponseDTO;
 import com.urgenciasYa.domain.model.UserEntity;
@@ -20,8 +22,10 @@ public interface UserMapper {
 
     UserResponseDTO userEntityToUserResponseDTO(UserEntity userEntity);
 
-    UserRegisterDTO userEntityToUserRegisterDTO(UserEntity userEntity);
+//    UserRegisterDTO userEntityToUserRegisterDTO(UserEntity userEntity);
 
     UserEntity userUpdateDTOToUserEntity(UserUpdateDTO userUpdateDTO);
+
+    String epsResponseDTOToUserEntity(EpsResponseDTO epsResponseDTO);
 
 }
