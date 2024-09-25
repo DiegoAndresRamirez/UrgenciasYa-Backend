392bff4941de25fecd0f7006d05a9a11141dbff4
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-25 05:33:18 -0500

    create the comment of the package handleError and delete useless imports

--- src/main/java/com/urgenciasYa/infrastructure/handleError/SuccessResponse.java
+++ src/main/java/com/urgenciasYa/infrastructure/handleError/SuccessResponse.java
@@ -4,11 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+// Class to represent a successful response
 @Getter
 @Setter
 @Builder
 public class SuccessResponse {
-    private Integer code;
-    private String status;
-    private String message;
+    private Integer code;  // HTTP status code of the success response
+    private String status; // Status description of the response (e.g., "OK")
+    private String message; // Message providing additional details about the response
 }
