74687dc6196d97d44dc6b8c5c38a7b007d8327ff
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-25 00:23:13 -0500

    Add userMapper with methods

--- src/main/java/com/urgenciasYa/application/mappers/UserMapper.java
+++ src/main/java/com/urgenciasYa/application/mappers/UserMapper.java
@@ -1,4 +1,27 @@
 package com.urgenciasYa.application.mappers;
 
+import com.urgenciasYa.application.dto.request.UserRegisterDTO;
+import com.urgenciasYa.application.dto.request.UserRegisterRequestDTO;
+import com.urgenciasYa.application.dto.request.UserUpdateDTO;
+import com.urgenciasYa.application.dto.response.LoginDTO;
+import com.urgenciasYa.application.dto.response.UserResponseDTO;
+import com.urgenciasYa.domain.model.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserEntity userRegisterRequestDTOtoUserEntity(UserRegisterRequestDTO userRegisterRequestDTO);
+
+    LoginDTO userEntityToLoginDTO(UserEntity userEntity);
+
+    UserResponseDTO userEntityToUserResponseDTO(UserEntity userEntity);
+
+    UserRegisterDTO userEntityToUserRegisterDTO(UserEntity userEntity);
+
+    UserEntity userUpdateDTOToUserEntity(UserUpdateDTO userUpdateDTO);
+
 }
