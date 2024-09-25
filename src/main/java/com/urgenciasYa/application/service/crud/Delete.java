bb07eb4a8bd9382f294cd3e8a5f5dd7e8d970bea
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-25 03:54:27 -0500

    create the comment of the package crud and delete useless imports

--- src/main/java/com/urgenciasYa/application/service/crud/Delete.java
+++ src/main/java/com/urgenciasYa/application/service/crud/Delete.java
@@ -1,5 +1,12 @@
 package com.urgenciasYa.application.service.crud;
 
+/*
+ * Generic interface for deleting entities.
+ */
+
 public interface Delete <ID>{
-    void delete(ID id);
+
+    //Deletes an entity identified by the given ID.
+
+    void delete(ID id); // Method to delete an entity by its ID
 }
--- src/main/java/com/urgenciasYa/application/service/crud/ReadById.java
+++ src/main/java/com/urgenciasYa/application/service/crud/ReadById.java
@@ -1,5 +1,12 @@
 package com.urgenciasYa.application.service.crud;
 
+/*
+ * Generic interface for reading an entity by its ID.
+ */
+
 public interface ReadById <Entity, ID> {
-    Entity getById(ID id);
+
+    //Retrieves an entity by its ID.
+
+    Entity getById(ID id); // Method to get an entity by its ID
 }
